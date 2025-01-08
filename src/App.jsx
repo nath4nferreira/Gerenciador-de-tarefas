ff7aa9ea1e84d4f1c1479c662ec5f60d89165457
nath4nferrira <eusounear@icloud.com>
2025-01-08 09:39:11 -0300

    Task-list adicionada

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Titulo teste 1",
+      description:"teste",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Titulo teste 2",
+      description:"teste",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Titulo teste 3",
+      description:"teste",
+      isCompleted: false,
+    },
+  ])
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de tarefas
+        </h1>
+        <AddTask />
+        <Tasks tasks={tasks} />
+        </div>
+    </div>
+  );
 }
 
-export default App
+export default App;

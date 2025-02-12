d6707e56e8072c50a691355bec71b83ebfaf6908
nath4nferrira <eusounear@icloud.com>
2025-02-12 19:02:11 -0300

    adicionando tarefas

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,29 @@
-function AddTask(){
-    return <h1>AddTask</h1>
+import { useState } from "react"
+
+function AddTask({onAddTaskSubmit}){
+    const [title, setTitle] = useState("12")
+    const [description, setDescription] = useState("")
+    return (
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+        <input 
+        type="text" 
+        placeholder="Digite o título da tarefa"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+        />
+        <input 
+        type="text" 
+        placeholder="Digite a descrição da tarefa" className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+        />
+        <button 
+        onClick={() => onAddTaskSubmit(title,description)} 
+        className="bg-slate-500 text-white px-4 py-2 rounded-md">
+            Adicionar
+        </button>
+    </div>)
 }
 
 export default AddTask

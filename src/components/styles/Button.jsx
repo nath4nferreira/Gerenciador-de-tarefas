3e1ff6c3546cd1a733386df6c4eed9ee8b124563
Nathan <nathancosta2006@gmail.com>
2025-04-24 14:45:10 -0300

    ajuste de style

--- src/components/styles/Button.jsx
+++ src/components/styles/Button.jsx
@@ -1,5 +1,6 @@
 function Button(props) {
-    return <button {...props}  className="bg-slate-400 p-2 rounded-md text-white">
+    return <button {...props}  className="bg-slate-400 p-2 rounded-md text-white border px-4 py-2 transition duration-300 ease-in-out
+         hover:bg-blue-400 hover:border-blue-400  hover:shadow-[0_0_10px_#60a5fa,0_0_20px_#60a5fa,0_0_30px_#60a5fa]">
         {props.children}
     </button>
 }
